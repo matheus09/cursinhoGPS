9a104dcce9b0976d64d60ee83169bdf26ee6aed7
Jucylene Melo <jucylenemelo@gmail.com>
2013-10-27 17:46:10 -0200

    Últimas mudanças!
    Signed-off-by: Jucylene Melo <jucylenemelo@gmail.com>

---
+++ src/java/modelo/Pessoa.java
@@ -0,0 +1,308 @@
+package modelo;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author jucylene
+ */
+@MappedSuperclass
+public class Pessoa implements Serializable {
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String telefone;
+    @Column(nullable = false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataNascimento;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String rua;
+    @Column(nullable = false)
+    private String numero;
+    @Column(nullable = false)
+    private String complemento;
+    @Column(nullable = false)
+    private String bairro;
+    @Column(nullable = false)
+    private String pontoReferencia;
+    @Column(nullable = false)
+    private String cep;
+    @Column(nullable = false)
+    private String cidade;
+    @Column(nullable = false)
+    private String estado;
+      
+    
+    /**
+     * (Não implementado) Testa se a Pessoa é maior de idade.
+     * @return (está retornando false, por enquanto) indica true se for maior de idade ou false, caso contrário.
+     */
+    public boolean maioridade(){
+        Calendar nasc = Calendar.getInstance();
+        nasc.setTime(dataNascimento);
+        Calendar agora = Calendar.getInstance();
+        agora.setTime(new Date());
+        agora.roll(Calendar.YEAR, -18);
+        
+        if (agora.after(nasc)) {
+            return true;
+        } else {
+            return false;
+        }
+        
+        /*
+        if ((agora.YEAR - nasc.YEAR) > 18) {
+            return true;
+        } else if ((agora.YEAR - nasc.YEAR) < 18) {
+            return false;
+        } else {
+            if (agora.MONTH > nasc.MONTH) {
+                return true;
+            } else if (ago
+            * ra.MONTH < nasc.MONTH) {
+                return false;
+            } else {
+                if (agora.DAY_OF_MONTH > nasc.DAY_OF_MONTH) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }*/
+    }
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the dataNascimento
+     */
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento a data de nascimento da pessoa.
+     */
+    public void setDataNascimento(Date dataNascimento) {
+        SimpleDateFormat df = new SimpleDateFormat("DD/MM/yyyy");
+        df.format(dataNascimento);
+        this.dataNascimento = dataNascimento;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+
+    /**
+     * @return the endereco
+     */
+    
+    /**
+     * @return the rua
+     */
+    public String getRua() {
+        return rua;
+    }
+
+    /**
+     * @param rua the rua to set
+     */
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    /**
+     * @return the numero
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the complemento
+     */
+    public String getComplemento() {
+        return complemento;
+    }
+
+    /**
+     * @param complemento the complemento to set
+     */
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    /**
+     * @return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return the pontoReferencia
+     */
+    public String getPontoReferencia() {
+        return pontoReferencia;
+    }
+
+    /**
+     * @param pontoReferencia the pontoReferencia to set
+     */
+    public void setPontoReferencia(String pontoReferencia) {
+        this.pontoReferencia = pontoReferencia;
+    }
+
+    /**
+     * @return the cep
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+   
+   
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pessoa)) {
+            return false;
+        }
+        Pessoa other = (Pessoa) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Pessoa[ id=" + id + " ]";
+    }
+
+    
+
+    
+}
